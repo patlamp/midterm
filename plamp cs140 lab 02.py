e8cb0276dd4a3751d54fdd9c755e7edf3c9d5be7
Patrick Lamp <plamp@olivetcollege.edu>
2012-04-25 13:51:46 -0400

    added lab 02

---
+++ plamp cs140 lab 02.py
@@ -0,0 +1,31 @@
+# Lab 02
+# Patrick Lamp
+# 7/12/2012
+# Directions
+user = raw_input('Which direction? ')
+if user == 'N':
+    print 'You travel North for a bit'
+
+elif user == 'S':
+    print 'You travel South for a bit'
+
+elif user == 'E':
+    print 'You travel East for a bit'
+
+elif user == 'W':
+    print 'You travel West for a bit'
+
+elif user == 'NE':
+    print 'You travel NorthEast for a bit'
+
+elif user == 'SE':
+    print 'You travel SouthEast for a bit'
+
+elif user == 'SW':
+    print 'You travel SouthWest for a bit'
+
+elif user == 'NW':
+    print 'You travel NorthWest for a bit'
+    
+else:
+    print 'Invalid input'
