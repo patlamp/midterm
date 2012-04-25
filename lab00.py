c97c967ec60aa0481173754923159f9e9707a43f
Patrick Lamp <plamp@olivetcollege.edu>
2012-04-25 13:42:15 -0400

    first commit

---
+++ lab00.py
@@ -0,0 +1,47 @@
+# lab00.py
+# Patrick Lamp
+# 2012-01-23
+# Version: 1
+# This is the comment block of Lab 00
+
+#Hello World
+print 'Hello, World!'
+
+print "  "
+#Top 100 Songs of 1993
+print 'Number', 'Title', 'Artist'
+print
+print '1', 'I Will Always Love You', 'Whitney Houston'
+print
+print '2', 'Whoomp! (There It Is)', 'Tag Team'
+print
+print '3', "Can't Help Falling In Love", 'UB40'
+print
+print '4', "That's The Way Love Goes", 'Janet Jackson'
+print
+print '5', 'Freak', 'Silk'
+print
+print '6', 'Weak', 'SWV'
+print
+print '7', 'If I Ever Fall In Love', 'Shai'
+print
+print '8', 'Dreamlover', 'Mariah Carey'
+print
+print '9', 'Rump Shaker', 'Wreckx-N-Effect'
+print
+print '10', 'Informer', 'Snow'
+
+
+print "  "
+#Interactive Greeting
+firstname = raw_input('what\'s your name? ')
+age = raw_input('What is your age? ')
+sex = raw_input('What is your sex? ')
+location = raw_input('What is your location? ')
+print 'Hello', firstname
+print 'Your age is: ', age
+print 'Your sex is: ', sex
+print 'Your location is: ', location
+
+
+
